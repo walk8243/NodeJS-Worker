f78625a56b860911ea00017c0ecec4209f2a6ee0
walk8243 <walk8243@gmail.com>
2019-08-02 20:08:42 +0900

    MessageChannelを使ってみる

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log('Node.JS Worker.');
+import { MessageChannel } from "worker_threads";
+
+// const messageChannel = new MessageChannel();
+// const port1 = messageChannel.port1;
+// const port2 = messageChannel.port2;
+const { port1, port2 } = new MessageChannel();
+port1.on('message', (message) => { console.log('received', message); });
+port2.postMessage({ foo: 'bar' });
