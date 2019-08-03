0afbde1a350b5b7a0e582e14424a0ffdd875a074
walk8243 <walk8243@gmail.com>
2019-08-03 19:04:41 +0900

    直列用のコードを作成

---
+++ src/normal.ts
@@ -0,0 +1,19 @@
+const data = [];
+const loop = 4;
+
+for(let i=0; i<loop; i++) {
+  const naturalNumber = [];
+  target: for(let i=2; i<100000; i++) {
+    check: for(let j=2; j<i; j++) {
+      if(i % j == 0) {
+        continue target;
+      }
+    }
+    naturalNumber.push(i);
+  }
+  
+  data.push({ result: naturalNumber });
+}
+
+// console.log(data);
+console.log(data.map((value) => value.result.length));
