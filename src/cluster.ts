09375e11a821d673f1d2bcd63f5ae8a084f9a5cb
walk8243 <walk8243@gmail.com>
2019-08-03 15:53:13 +0900

    ファイル名を変更して、threadIdやpidを取得できるように。

--- src/cluster.ts
+++ src/cluster.ts
@@ -1,14 +1,14 @@
 import cluster, { worker } from "cluster";
 
-console.log('common');
+console.log('common', process.pid);
 if(cluster.isMaster) {
-  console.log('main cluster');
+  console.log('main cluster', process.pid);
   cluster.fork();
   cluster.on('message', (worker, message, handle) => {
     console.log('received', message, 'from', worker.id);
   });
 } else {
-  console.log('worker cluster');
+  console.log('worker cluster', process.pid);
   worker.send({ foo: 'bar' });
   worker.kill();
 }
--- src/worker.ts
+++ src/worker.ts
@@ -1,12 +1,12 @@
-import { Worker, isMainThread, parentPort } from "worker_threads";
+import { Worker, isMainThread, threadId, parentPort } from "worker_threads";
 
-console.log('common');
+console.log('common', threadId);
 if(isMainThread) {
-  console.log('main thread');
+  console.log('main thread', threadId);
   const worker = new Worker(__filename);
   worker.on('message', (message) => { console.log('received', message); });
   worker.on('error', (error) => { console.error('occurred', error.message); });
 } else {
-  console.log('worker thread');
+  console.log('worker thread', threadId);
   parentPort!.postMessage({ foo: 'bar' });
 }
